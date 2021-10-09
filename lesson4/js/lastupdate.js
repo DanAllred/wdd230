4d5a6b7bcb26e4ff83ae67b7e0f2f1fcc8e3c5bb
DanAllred <danielallred58@gmail.com>
2021-10-09 10:30:21 -0400

    final push

--- lesson4/js/lastupdate.js
+++ lesson4/js/lastupdate.js
@@ -2,10 +2,10 @@ const d = new Date();
 const year = d.getFullYear();
 const fulldate = `${year}`;
 document.querySelector("#dateyear").textContent = fulldate;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const fdate = d.toLocaleDateString("en-UK", options);
+document.querySelector("#lastUpdated").textContent = fdate;
 
-
-let oLastModif = new Date(document.lastModified)
-document.querySelector("#lastUpdated").textContent = oLastModif;
 
 
 function toggleMenu() {
