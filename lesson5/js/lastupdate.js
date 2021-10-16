511c3913852c25ef1b76dacfd974f7026f264fd5
DanAllred <danielallred58@gmail.com>
2021-10-16 12:58:08 -0400

    LESSON5

---
+++ lesson5/js/lastupdate.js
@@ -0,0 +1,16 @@
+const d = new Date();
+const year = d.getFullYear();
+const fulldate = `${year}`;
+document.querySelector("#dateyear").textContent = fulldate;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const fdate = d.toLocaleDateString("en-UK", options);
+document.querySelector("#lastUpdated").textContent = fdate;
+
+
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+
+
