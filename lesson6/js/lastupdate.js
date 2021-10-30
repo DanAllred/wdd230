0b4c2fedc9a6318730a3e4fc360467fc7ce7e736
DanAllred <danielallred58@gmail.com>
2021-10-30 13:25:56 -0400

    updating to test

--- lesson6/js/lastupdate.js
+++ lesson6/js/lastupdate.js
@@ -20,3 +20,15 @@ WebFont.load({
     }
   });
 
+//Last Visit
+const lastVisit = localStorage.getItem('lastVisit') || today; 
+const displayDate = '';
+const millisecondsToDays = 8640000;
+if (lastVisit == today) {
+    document.querySelector('#galleryVisits').textContent = `Last visit: First timer! &#x1F609;`;
+}
+else {
+    displayDate = (lastVisit - today / millisecondsToDays).toFixed(0);
+    document.querySelector('#galleryVisits').textContent =  `Last visit: ${displayDate}`;
+}
+ localStorage.setItem('lastVisit', today); 
