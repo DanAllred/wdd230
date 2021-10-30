2cb63f0747846b072db593d72a095ad5f371d6c6
DanAllred <danielallred58@gmail.com>
2021-10-30 15:51:38 -0400

    update

--- lesson7/js/lastupdate.js
+++ lesson7/js/lastupdate.js
@@ -1,22 +1,45 @@
-const d = new Date();
-const year = d.getFullYear();
-const fulldate = `${year}`;
-document.querySelector("#dateyear").textContent = fulldate;
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const fdate = d.toLocaleDateString("en-UK", options);
-document.querySelector("#lastUpdated").textContent = fdate;
+let d = new Date();
+document.getElementById("copyrightYear").textContent = d.getFullYear();
+
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+}
+
+document.getElementById("currentDate").textContent = d.toLocaleDateString('en-UK', options);
 
 
-
+//
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
+//
+
+
+//datesbetween
+const lastVisit = localStorage.getItem('lastVisit') || today; 
+//const displayDate = '';
+const millisecondsToDays = 86400000;
+if (lastVisit == today) {
+    document.querySelector('#galleryVisits').textContent = `Last visit: Today is your first visit!`;
+}
+else {
+    displayDate = (lastVisit - today / millisecondsToDays).toFixed(0);
+    document.querySelector('#galleryVisits').textContent =  `Last visit: ${displayDate}`;
+}
+ localStorage.setItem('lastVisit', today); 
+
+
+//
 WebFont.load({
-    google: {
-      families: [
-         'Oswald', 'sans serif'
-      ]
-    }
-  });
+  google: {
+    families: [
+       'Oswald', 'sans serif'
+    ]
+  }
+});
+
 
--- lesson7/js/lastvisit.js
+++ lesson7/js/lastvisit.js
@@ -1,11 +1,10 @@
-const lastVisit = localStorage.getItem('lastVisit') || today; 
+const lastVisit = localStorage.getItem('lastVisit') || today;
 const displayDate = '';
 const millisecondsToDays = 8640000;
 if (lastVisit == today) {
-    document.querySelector('#galleryVisits').textContent = `Last visit: First timer! &#x1F609;`;
-}
-else {
+    document.querySelector('#galleryVisits').textContent = `Last visit: Today is your first visit!`;
+} else {
     displayDate = (lastVisit - today / millisecondsToDays).toFixed(0);
-    document.querySelector('#galleryVisits').textContent =  `Last visit: ${displayDate}`;
+    document.querySelector('#galleryVisits').textContent = `Last visit: ${displayDate}`;
 }
- localStorage.setItem('lastVisit', today); 
+localStorage.setItem('lastVisit', today);
