fe63da17b6c953f7bc13f9bbb1e7c6a994a8020b
DanAllred <danielallred58@gmail.com>
2021-11-19 14:30:34 -0500

    lesson10

---
+++ lesson10/js/lazyload.js
@@ -0,0 +1,34 @@
+const seenImages = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: '0px 0px 500px 0px'
+};
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  }
+};
+
+if ('InstersectionObserver' in window) {
+  const imgObserver = new IntersectionObserver((items, imgObserver) => {
+    items.forEach(item => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
+    })
+  }, imgOptions);
+
+  seenImages.forEach(img => {
+    imgObserver.observe(img);
+  });
+}
+else {
+  seenImages.forEach(img => {
+    loadImages(img);
+  });
+}
+
